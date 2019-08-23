c54d1f7429c283445419f0225374210d3e1d058c
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2019-08-24 00:53:30 +0600

    receiver uses auto switch

--- app/Commands/DefaultMqReceiver.php
+++ app/Commands/DefaultMqReceiver.php
@@ -11,32 +11,38 @@ class DefaultMqReceiver extends Command
 {
     use AmqpConnectionChannel;
 
-    protected $signature = 'default:receiver';
+    protected $signature = 'default:receiver {--auto}';
 
     protected $description = 'RabbitMQ default receiver';
 
     public function handle () {
-        $queueName = trim($this->ask('Write a queue name: '));
-        if (empty($queueName)) {
-            $queueName = 'default-exchange-queue';
+        if (!$this->option('auto')) {
+            $qn = trim($this->ask('Write a queue name: '));
+        }
+
+        if (empty($qn)) {
+            $qn = 'default-exchange-queue';
         }
 
         /* @var AMQPStreamConnection $connection */
         /* @var \PhpAmqpLib\Channel\AMQPChannel $channel */
         [ $connection, $channel ] = $this->setup();
-        $channel->queue_declare($queueName, false, true, false, false);
+        $channel->queue_declare($qn, false, true, false, false);
         $i = 0;
 
         $callback = function (AMQPMessage $msg) use (&$i) {
-            echo sprintf("[%'*5d] %s%s", ++$i, $msg->body, PHP_EOL);
+            $this->output->success(sprintf("[%'*5d] %s%s", ++$i, $msg->body, PHP_EOL));
         };
 
         $channel->basic_qos(null, 1, null);
-        $channel->basic_consume($queueName, '', false, true, false, false, $callback);
+        $channel->basic_consume($qn, '', false, true, false, false, $callback);
+
+        $this->output->warning('Waiting for messages [' . $qn . ']');
 
         while ( $channel->is_consuming() ) {
             $channel->wait();
         }
+
         $channel->close();
         $connection->close();
     }
