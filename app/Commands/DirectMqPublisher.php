cbeb24f2bca6f26749d1ed91cedd19be5e042d06
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2019-08-24 01:06:10 +0600

    direct publish & consume added

---
+++ app/Commands/DirectMqPublisher.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Commands;
+
+use App\Extensions\AmqpConnectionChannel;
+use Faker\Factory;
+use LaravelZero\Framework\Commands\Command;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class DirectMqPublisher extends Command
+{
+    use AmqpConnectionChannel;
+
+    protected $signature = 'direct:sender {--auto}';
+
+    protected $description = 'RabbitMQ direct sender';
+
+    public function handle () {
+        $auto = $this->option('auto');
+
+        if (!$auto) {
+            $text = trim($this->ask('Write any message: '));
+            $ex = trim($this->ask('Write an exchange name: '));
+            $r = trim($this->ask('Write a routing key: '));
+        }
+
+        if (empty($text)) {
+            $text = json_encode([
+                'name'  => Factory::create()->name,
+                'email' => Factory::create()->email,
+            ]);
+        }
+
+        if (empty($ex)) {
+            $ex = 'direct.exchange';
+        }
+
+        if (empty($r)) {
+            $r = 'direct.routing.key';
+        }
+
+        $message = new AMQPMessage($text, [ 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT ]);
+
+        /* @var AMQPStreamConnection $connection */
+        /* @var \PhpAmqpLib\Channel\AMQPChannel $channel */
+        [ $connection, $channel ] = $this->setup();
+        $channel->exchange_declare($ex, 'direct', false, true, false);
+
+        $channel->basic_publish($message, $ex, $r);
+
+        $this->output->success(sprintf('Sent message: [exchange: %s] [routing: %s] - [%s]', $ex, $r, $text));
+
+        $channel->close();
+        $connection->close();
+    }
+}
---
+++ app/Commands/DirectMqReceiver.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Commands;
+
+use App\Extensions\AmqpConnectionChannel;
+use LaravelZero\Framework\Commands\Command;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class DirectMqReceiver extends Command
+{
+    use AmqpConnectionChannel;
+
+    protected $signature = 'direct:receiver {--auto}';
+
+    protected $description = 'RabbitMQ direct receiver';
+
+    public function handle () {
+        if (!$this->option('auto')) {
+            $ex = trim($this->ask('Write an exchange name: '));
+            $qn = trim($this->ask('Write a queue name: '));
+            $bk = trim($this->ask('Write a binding key: '));
+        }
+
+        if (empty($ex)) {
+            $ex = 'direct.exchange';
+        }
+
+        if (empty($bk)) {
+            $bk = 'direct.routing.key';
+        }
+
+        if (empty($qn)) {
+            $qn = 'direct.exchange.queue';
+        }
+
+        /* @var AMQPStreamConnection $connection */
+        /* @var \PhpAmqpLib\Channel\AMQPChannel $channel */
+        [ $connection, $channel ] = $this->setup();
+        $channel->queue_declare($qn, false, true, false, false);
+        $channel->queue_bind($qn, $ex, $bk);
+
+        $callback = function (AMQPMessage $msg) use ($qn, $ex, $bk) {
+            $this->output->success(sprintf("[ex: %s] [queue: %s] [bind: %s] - [MSG: %s]", $ex, $qn, $bk, $msg->body));
+        };
+
+        $channel->basic_consume($qn, '', false, true, false, false, $callback);
+
+        $this->output->warning(sprintf('Waiting for messages [%s] - [%s] - [%s]', $ex, $qn, $bk));
+
+        while ( $channel->is_consuming() ) {
+            $channel->wait();
+        }
+
+        $channel->close();
+        $connection->close();
+    }
+}
