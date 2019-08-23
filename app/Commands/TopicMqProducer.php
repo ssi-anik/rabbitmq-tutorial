6f49935492c7475f03b137688946f6ee54ed52e7
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2019-08-24 01:18:32 +0600

    topic producer & consumer added

---
+++ app/Commands/TopicMqProducer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Commands;
+
+use App\Extensions\AmqpConnectionChannel;
+use Faker\Factory;
+use LaravelZero\Framework\Commands\Command;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class TopicMqProducer extends Command
+{
+    use AmqpConnectionChannel;
+
+    protected $signature = 'topic:publisher {--auto}';
+
+    protected $description = 'RabbitMQ topic producer';
+
+    public function handle () {
+        $auto = $this->option('auto');
+
+        if (!$auto) {
+            $text = trim($this->ask('Write any message: '));
+            $ex = trim($this->ask('Write an exchange name: '));
+            $r = trim($this->ask('Write a routing key: '));
+        }
+
+        if (empty($text)) {
+            $text = json_encode([
+                'name'  => Factory::create()->name,
+                'email' => Factory::create()->email,
+            ]);
+        }
+
+        if (empty($ex)) {
+            $ex = 'topic.exchange';
+        }
+
+        if (empty($r)) {
+            $r = 'topic.routing.key';
+        }
+
+        $message = new AMQPMessage($text, [ 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT ]);
+
+        /* @var AMQPStreamConnection $connection */
+        /* @var \PhpAmqpLib\Channel\AMQPChannel $channel */
+        [ $connection, $channel ] = $this->setup();
+        $channel->exchange_declare($ex, 'topic', false, true, false);
+
+        $channel->basic_publish($message, $ex, $r);
+
+        $this->output->success(sprintf('Sent message: [exchange: %s] [routing: %s] - [%s]', $ex, $r, $text));
+
+        $channel->close();
+        $connection->close();
+    }
+}
